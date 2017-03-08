b2a259b3f5feb1ac6eb3f12c62a3b039be0ca282
Kyle Chang Fatt <kchangfatt@gmail.com>
2017-03-08 10:37:01 -0500

    Added rune image and other stats to stats component.

---
+++ src/app/types/rune/image.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by kyle on 3/8/2017.
+ */
+
+export class Image{
+  full: string;
+  sprite: string;
+  group: string;
+}
--- src/app/types/rune/rune.ts
+++ src/app/types/rune/rune.ts
@@ -1,4 +1,6 @@
 import {Stats} from "./stats";
+import {Image} from "./image";
+import {RuneType} from "./rune.type";
 /**
  * Created by kyle on 2/26/2017.
  */
@@ -7,5 +9,7 @@ export class Rune{
   name: string;
   tags: string[];
   stats: Stats;
+  image: Image;
+  rune: RuneType;
   sanitizedDescription: string;
 }
