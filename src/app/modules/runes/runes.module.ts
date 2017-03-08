856d77c2d61b36bc69728da9aeb0941ab02ec3fb
Kyle Chang Fatt <kchangfatt@gmail.com>
2017-03-07 21:41:54 -0500

    manually tested selection of rune

---
+++ src/app/modules/runes/runes.module.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by kyle on 3/4/2017.
+ */
+import {NgModule} from "@angular/core";
+import {HttpModule} from "@angular/http";
+import {Pipes} from "../pipes/pipes.module";
+
+@NgModule({
+  declarations: [],
+  imports: [HttpModule, Pipes],
+  exports: []
+
+})
+export class RunesModule{
+
+}
--- src/app/runes-section/runes-section.component.ts
+++ src/app/runes-section/runes-section.component.ts
@@ -2,7 +2,11 @@
  * Created by kyle on 3/2/2017.
  */
 
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Http} from "@angular/http";
+import {RuneGroup} from "../types/rune/rune.group";
+import {RunesService} from "../modules/runes-services/services/runes.service";
+import {Rune} from "../types/rune/rune";
 
 @Component({
   moduleId: module.id,
@@ -10,9 +14,27 @@ import {Component} from "@angular/core";
   templateUrl: 'runes-section.component.html',
 })
 
-export class RunesSectionComponent{
+export class RunesSectionComponent implements OnInit{
 
-  constructor(){}
+  private runes: RuneGroup;
 
+  private selectedRune: Rune;
+
+  constructor(private http: Http, private runesService: RunesService){}
+
+  getAllRunes(): void{
+    this.runesService.getAllRunes()
+      .then(runes => {
+        this.runes = runes;
+      });
+  }
+
+  selectRune(rune: Rune): void{
+    this.selectedRune = rune;
+  }
+
+  ngOnInit(){
+    this.getAllRunes();
+  }
 }
 
